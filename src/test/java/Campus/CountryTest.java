da9f6a91d750e15d38d4c951747899b826ccb413
Belgin <belgin.doan@gmail.com>
2023-05-01 22:03:21 -0400

    status code edited

--- src/test/java/Campus/CountryTest.java
+++ src/test/java/Campus/CountryTest.java
@@ -119,7 +119,7 @@ public class CountryTest {
                 .delete("/school-service/api/countries/{countryId}")
 
                 .then()
-                .statusCode(201);
+                .statusCode(200);
     }
 
     /** delete country negative test */
